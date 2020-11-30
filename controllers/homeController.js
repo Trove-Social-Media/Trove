a755912a3ce3bfee2951f6095ba438ef8a23e544
twong17 <70173433+twong17@users.noreply.github.com>
2020-11-30 18:42:04 -0500

    Update homeController.js

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,6 +3,7 @@ const multer = require('multer');
 const helpers = require('../helpers');
 var UserInfo = require('../models/userinfo');
 var Post = require('../models/post');
+var Comment = require('../models/comment');
 const path = require('path');
 const fs = require('fs');
 const bodyParser = require('body-parser');
@@ -32,11 +33,13 @@ exports.home = function(req, res) {
 
     //This method is intended to show all posts of logged in users' friends
     Post.find({user: {"$in": req.session.user.friends}})
+    .populate('user')
     .exec(function (err, list_posts) {
       if (err) { return next(err); }
       //Successful, so render
         console.log(list_posts);
       res.render('home', { title: 'Trove User Profile Page', user: req.session.user, post_list: list_posts});
+        
     });
 };
 
@@ -46,90 +49,7 @@ exports.home_create_get = function(req, res) {
     res.send("User Profile GET");
 };
 
-//POST function to create a post
-exports.home_create_post = function(req, res) {
-    //Store req post content
-    var postcontent = req.body.postcontent;
-    if(postcontent == null){
-        res.send("Empty post");
-    }
-    else{
-        //res.send(postcontent);
-        //Create a new post object 
-    var post = new Post(
-      { content: req.body.postcontent,
-        user:req.session.user,
-        //song: {path: trimmedPath}
-      }
-    );
-      post.save(function (err) {
-               if (err) { return next(err); }
-               // state that the user has been sucessfully created
-               
-                res.send("Success");
-             }); 
 
-      console.log(post);
-    }    
-    
-};
-
-//POST function to create an audio post
-exports.home_create_audio_post = function(req, res) {
-    let upload = multer({ storage: storage, fileFilter: helpers.imageFilter }).single('profile_pic');
-
-    upload(req, res, function(err) {
-        if (req.fileValidationError) {
-            return res.send(req.fileValidationError);
-        }
-        else if (!req.file) {
-            return res.send('Please select an image to upload');
-        }
-        else if (err instanceof multer.MulterError) {
-            return res.send(err);
-        }
-        else if (err) {
-            return res.send(err);
-        }
-
-        var trimmedPath = req.file.path.slice(6);
-        var filePath = req.file.path;
-        audioPath = trimmedPath;
-        console.log("The path is: " + trimmedPath);
-        audioName = req.file.originalname;
-        req.session.audioPath = audioPath;
-        req.session.audioName = audioName;
-        res.render('audioText', {name: req.session.audioName, path:req.session.audioPath});
-    });   
-};
-
-//POST function to create the text for audio post
-exports.home_create_audio_text = function(req, res) {
-    //Store req post content
-    var postcontent = req.body.postcontent;
-    if(postcontent == null){
-        res.send("Empty post");
-    }
-    else{
-        //res.send(postcontent);
-        //Create a new post object 
-    var post = new Post(
-      { content: req.body.postcontent,
-        user:req.session.user,
-        song: {path: req.session.audioPath}
-      }
-    );
-      post.save(function (err) {
-               if (err) { return next(err); }
-               // state that the user has been sucessfully created
-               
-                res.send("Success");
-             }); 
-
-      console.log(post);
-    }    
-    
-};
 
 // POST method to like a post
 exports.home_create_like = async function(req, res) {
@@ -150,21 +70,16 @@ exports.home_create_like = async function(req, res) {
 };
 
 // POST method to create a comment
-exports.home_create_comment = async function(req, res) {
+exports.home_create_comment = function(req, res) {
     //res.send(req.body.action);
-        var post;
-      await Post.findById(req.body.action, function(err,pro){
-          post=pro;
-          console.log("We found " + post);
+        //var post;
+      Post.findById(req.body.action, function(err,pro){
+          //post=pro;
+          req.session.currentPost = pro;
+          res.redirect('/catalog/comment');
         });
     
-        var string = "Some comment"
     
-        Post.findOneAndUpdate({ _id: req.body.action }, { $push: { comments: string } },function(err, response) {
-         if (err) {
-         callback(err);
-        } else {
-         res.send("Added a comment that says " + string);
-        }
-        });
+    
+
 };
